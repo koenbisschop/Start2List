4a889922f4775b458f4c5449a337327bfa222e57
koenbisschop <koen.bisschop@hotmail.be>
2019-11-20 11:18:35 +0100

    Even worpen met lijsten

---
+++ Start2List/DobbelenPlus/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DobbelenPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //je creëert een array met 10 willekeurige worpen (1..6)
+            //haal de even worpen uit de array via een list<int>
+            //toon de even worpen
+        }
+    }
+}
